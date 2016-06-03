a9a17fe50de6137111f273abfe6411ca6f022a8a
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-03 12:32:01 -0400

    Adding to pull

--- mister/views.py
+++ mister/views.py
@@ -1,15 +1,29 @@
+import csv
 from django.shortcuts import render
 from django.http import HttpResponse
 from mister.models import Collector
 
+
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+
 
 def collection(request):
-	data=Collector.objects.all().order_by('-time_collected')
-	return render(request, 'plantinfo.html', {'data': data,})
+	data = Collector.objects.all().order_by('-time_collected')
+	return render(request, 'plantinfo.html', {'data': data, })
 
 # def instance_detail(request, id):
 # 	instance=Collector.objects.get(id=id)
-# 	return render(request, 'instance_detail.html', {'instance': instance,})	
+# 	return render(request, 'instance_detail.html', {'instance': instance,})
 # 	pass
+
+
+def data_csv(request):
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="data_csv.csv"'
+	# Create the HttpResponse object with the appropriate CSV header.
+
+	writer = csv.writer(response)
+	writer.writerow(Collector.objects.all())
+
+	return response
