a4ef74ffbe3359087fad351364c861a26da8d488
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-03 12:48:44 -0400

    Adding csv output

--- mister/urls.py
+++ mister/urls.py
@@ -6,10 +6,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name="about"),
-    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'),
+        name="about"),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
     url(r'^plantinfo/$', views.collection, name='plantinfo'),
-    # url(r'^instance/(?P<id>[-\w]+)/$', views.instance_detail, name='instance_detail'),
+    # url(r'^instance/(?P<id>[-\w]+)/$', views.instance_detail,
+    # name='instance_detail'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^csv/$', views.data_csv, name='data_csv'),
 ]
--- mister/views.py
+++ mister/views.py
@@ -1,6 +1,6 @@
-import csv
+import djqscsv
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Collector
 from rest_framework import viewsets
 from .serializers import CollectorSerializer
@@ -11,9 +11,8 @@ def index(request):
 
 
 def collection(request):
-	data = Collector.objects.all().order_by('-time_collected')
-	return render(request, 'plantinfo.html', {'data': data, })
-
+    data = Collector.objects.all().order_by('-time_collected')
+    return render(request, 'plantinfo.html', {'data': data, })
 
 
 class CollectionsViewSet(viewsets.ModelViewSet):
@@ -22,3 +21,15 @@ class CollectionsViewSet(viewsets.ModelViewSet):
     """
     queryset = Collector.objects.all().order_by('-pH_level')
     serializer_class = CollectorSerializer
+
+
+def data_csv(request):
+	qs = Collector.objects.all()
+	return djqscsv.render_to_csv_response(qs)
+
+# response = HttpResponse(content_type='text/csv')
+# response['Content-Disposition'] = 'attachment; filename="data_csv.csv"'
+# # Create the HttpResponse object with the appropriate CSV header.
+# writer = csv.writer(response)
+# writer.writerow(Collector.objects.all())
+# return response
