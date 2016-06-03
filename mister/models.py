454ac98f71e18d42f026c5cb5bb760a5c67dbb0d
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-03 15:51:30 -0400

    Create csv files

--- mister/models.py
+++ mister/models.py
@@ -9,6 +9,7 @@ class Collector(models.Model):
     temperature = models.FloatField()
     time_collected = models.DateTimeField(auto_now_add=True)
 
-   
+    def __str__(self):
+        return str(self.pH_level)
+
    # two separates models: temp and pH_levl
-   
--- mister/views.py
+++ mister/views.py
@@ -1,6 +1,6 @@
-import djqscsv
+import csv
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from .models import Collector
 from rest_framework import viewsets
 from .serializers import CollectorSerializer
@@ -24,12 +24,10 @@ class CollectionsViewSet(viewsets.ModelViewSet):
 
 
 def data_csv(request):
-	qs = Collector.objects.all()
-	return djqscsv.render_to_csv_response(qs)
-
-# response = HttpResponse(content_type='text/csv')
-# response['Content-Disposition'] = 'attachment; filename="data_csv.csv"'
-# # Create the HttpResponse object with the appropriate CSV header.
-# writer = csv.writer(response)
-# writer.writerow(Collector.objects.all())
-# return response
+	response = HttpResponse(content_type='text/csv')
+	response['Content-Disposition'] = 'attachment; filename="data_csv.csv"'
+	# Create the HttpResponse object with the appropriate CSV header.
+	writer = csv.writer(response)
+	for collector in Collector.objects.all():
+		writer.writerow([collector.pH_level, collector.temperature])
+	return response
