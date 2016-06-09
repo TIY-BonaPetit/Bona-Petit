22a578ca06947706b42e7e9060b360f3f8a95149
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-09 11:38:38 -0400

    Refactor unneeded imports

--- mister/views.py
+++ mister/views.py
@@ -1,16 +1,19 @@
+import csv
+
+from rest_framework import viewsets
 
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Collector, UserProfile
-import csv
-from rest_framework import viewsets
-from .serializers import CollectorSerializer
-from .forms import UserForm, UserProfileForm
 from django.template import RequestContext
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from mister.serializers import UserSerializer
+from django.contrib.auth.decorators import login_required
+
+from .serializers import CollectorSerializer, UserSerializer
+from .forms import UserForm, UserProfileForm
+from .models import Collector  # , UserProfile
+
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 
@@ -23,7 +26,8 @@ def index(request):
 
 
 def collection(request):
-    data = Collector.objects.filter(user=request.user).order_by('-time_collected')
+    filtered = Collector.objects.filter(user=request.user)
+    data = filtered.order_by('-time_collected')
     return render(request, 'plantinfo.html', {'data': data})
 
 
@@ -60,20 +64,24 @@ def register(request):
 
             registered = True
         else:
-            print (user_form.errors, profile_form.errors)
+            print(user_form.errors, profile_form.errors)
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
+        payload = {'user_form': user_form,
+                   'profile_form': profile_form,
+                   'registered': registered}
 
     return render_to_response(
             'register.html',
-            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
+            payload,
             context)
-        
-    
+
+
 @login_required
 def restricted(request):
     return HttpResponse("You're Logged In!")
+
 
 def user_login(request):
     context = RequestContext(request)
@@ -91,30 +99,30 @@ def user_login(request):
             else:
                 return HttpResponse("Your Bona Petite account is disabled.")
         else:
-            print ('Invalid login details: {}, {}'.format(username, password))
+            print('Invalid login details: {}, {}'.format(username, password))
             return HttpResponse("Invalid login details supplied.")
 
     else:
         return render_to_response('login.html', {}, context)
 
-from django.contrib.auth import logout
 
 @login_required
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect('index')
 
-from django.contrib.auth.models import User
 
 @login_required
 def profile(request):
     pass
-    
+
+
 def data_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="data_csv.csv"'
-    # Create the HttpResponse object with the appropriate CSV header.
     writer = csv.writer(response)
-    for collector in Collector.objects.all():
+    filtered = Collector.objects.filter(user=request.user)
+    data = filtered.order_by('-time_collected')
+    for collector in data:
         writer.writerow([collector.pH_level, collector.temperature])
     return response
