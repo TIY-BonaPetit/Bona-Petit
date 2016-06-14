6439f4c41afaa0649e71b6250d374e3accd3b62b
Byron Wall <byrondevonwall@gmail.com>
2016-06-14 10:47:25 -0400

    try again on transitions and etc

--- mister/static/js/scripts.js
+++ mister/static/js/scripts.js
@@ -142,16 +142,26 @@
        //set svg data as json payload
        var data = json.results;
 
-      //change x domain
-      // x.domain(d3.extent(data, dateFn));
+       var svg = d3.select("#temp-graph").transition()
 
-      //append temp data line
-      svg.append("svg:path")
-        .attr("d", line(data))
-        .style("stroke", "limegreen");
+      //change x domain
+      x.domain(d3.extent(data, dateFn));
+
+
+      svg.select(".line")   // change the line
+            .duration(750)
+            .attr("d", line(data))
+
+      var circles = svg.selectAll("circle").data(data)
+
+      circles.transition()
+      .attr("cx", function(d) { return x(dateFn(d)) })
+      .attr("cy", function(d){ return y(tempFn(d))})
+
+
 
       //append temp data as circles
-      svg.selectAll("circle").data(data).enter()
+      circles.enter()
         .append("svg:circle")
         .attr("r", 4)
         .attr("cx", function(d){ return x(dateFn(d))})
@@ -270,15 +280,23 @@
        var data = json.results;
 
        //change x domain
-      //  x.domain(d3.extent(data, dateFn));
+       x.domain(d3.extent(data, dateFn));
+
+       var ecg = d3.select("#ec-graph").transition();
 
       //append ec data line
-      ecg.append("svg:path")
-       .attr("d", ecLine(data))
-       .style("stroke", "limegreen");
+      ecg.select(".line")   // change the line
+            .duration(750)
+            .attr("d", line(data))
+
+      var circles = ecg.selectAll("circle").data(data)
+
+      circles.transition()
+      .attr("cx", function(d) { return x(dateFn(d)) })
+      .attr("cy", function(d){ return ecY(ecFn(d))})
 
      //append ec data as circles
-     ecg.selectAll("circle").data(data).enter()
+     circles.enter()
        .append("svg:circle")
        .attr("r", 4)
        .attr("cx", function(d){ return x(dateFn(d))})
