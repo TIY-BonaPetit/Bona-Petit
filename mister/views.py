7de2e6f3459f15b51a11ac6c194f41d973cee92d
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-15 03:50:13 -0400

    CSS styling nav buttons and change logout redirect

--- mister/views.py
+++ mister/views.py
@@ -113,7 +113,7 @@ def user_login(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect('index')
+    return HttpResponseRedirect('/')
 
 
 @login_required
