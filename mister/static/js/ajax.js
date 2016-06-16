9bcfc691ace415ee08728ef9583cdad48f1bc025
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-16 01:16:47 -0400

    Ajax script for updating plant info

--- mister/static/js/ajax.js
+++ mister/static/js/ajax.js
@@ -6,7 +6,6 @@ setInterval(function(){
             $('#tc').text(result.results[0].time_collected);
             $('#tmp').text(result.results[0].temperature);
             $('#ecl').text(result.results[0].ec_level);
-            console.log("set")
         }
     })
 }, 1000);
